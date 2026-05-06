be1300810f6ed4242a21982ecdc925e91bc1bbdf
Fernando <wimpo000777@gmail.com>
2026-05-07 01:02:08 +0200

    comentarios

--- bildyapp-api/jest.config.js
+++ bildyapp-api/jest.config.js
@@ -1,6 +1,9 @@
 export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  transform: {},
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }]
+  },
   setupFiles: ['./tests/env-setup.js'],
   testMatch: ['**/tests/**/*.test.js'],
   coverageDirectory: 'coverage',
@@ -12,4 +15,8 @@ export default {
     global: { lines: 70, branches: 50, functions: 70, statements: 70 },
   },
   testTimeout: 30000,
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
 };
